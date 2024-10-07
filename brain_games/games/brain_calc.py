d9965b42db5014d0e009e6a689e909724fc768d6
Dana Senko <dana.es@mail.ru>
2024-10-07 14:41:42 +0300

    add new functions

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,42 @@
+from brain_games.cli import welcome_user
+from random import randint, choice
+
+
+def main_game_calc():
+    user_name = welcome_user()
+    print('What is the result of the expression?')
+    counter = 0
+
+    while counter < 3:
+        number1 = randint(1, 10)
+        number2 = randint(1, 10)
+        operations = ['+', '-', '*']
+        operation = choice(operations)
+        print(f'Question: {number1} {operation} {number2}')
+        user_answer = input('Your answer: ').lower()
+
+        if operation == '+':
+            correct_answer = number1 + number2
+        elif operation == '-':
+            correct_answer = number1 - number2
+        else:
+            correct_answer = number1 * number2
+
+        if int(user_answer) == correct_answer:
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"'{user_answer}' is the wrong answer ;(. "
+            f"The correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {user_name}!")
+            break
+
+    if counter == 3:
+        print(f'Congratulation, {user_name}!')
+
+
+def main():
+    main_game_calc()
+
+if __name__ == "__main__":  
+    main()
---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,39 @@
+from brain_games.cli import welcome_user
+from random import randint
+
+
+def main_game_even():
+    user_name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    counter = 0
+
+    while counter < 3:
+        question = randint(1, 1000)
+        print(f"Question: {question}")
+        user_answer = input("Your answer: ").lower()
+
+        if question % 2 == 0:
+            correct_answer = "yes"
+        else:
+            correct_answer = "no"
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            counter += 1
+        else:
+            print(
+                f"'{user_answer}' is the wrong answer ;(. "
+                f"The correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {user_name}!")
+            break
+    if counter == 3:
+        print(f"Congratulation, {user_name}!")
+
+
+def main():
+    main_game_even()
+
+
+if __name__ == "__main__":
+    main()
