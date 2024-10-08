106fabc7c30413eb5bfda3118abaedeb41dbfbee
Dana Senko <dana.es@mail.ru>
2024-10-08 09:07:11 +0300

    Файл отредактирован согласно стандарту PEP8

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -4,39 +4,42 @@ from random import randint, choice
 
 def main_game_calc():
     user_name = welcome_user()
-    print('What is the result of the expression?')
+    print("What is the result of the expression?")
     counter = 0
 
     while counter < 3:
         number1 = randint(1, 10)
         number2 = randint(1, 10)
-        operations = ['+', '-', '*']
+        operations = ["+", "-", "*"]
         operation = choice(operations)
-        print(f'Question: {number1} {operation} {number2}')
-        user_answer = input('Your answer: ').lower()
+        print(f"Question: {number1} {operation} {number2}")
+        user_answer = input("Your answer: ").lower()
 
-        if operation == '+':
+        if operation == "+":
             correct_answer = number1 + number2
-        elif operation == '-':
+        elif operation == "-":
             correct_answer = number1 - number2
         else:
             correct_answer = number1 * number2
 
         if int(user_answer) == correct_answer:
-            print('Correct!')
+            print("Correct!")
             counter += 1
         else:
-            print(f"'{user_answer}' is the wrong answer ;(. "
-            f"The correct answer was '{correct_answer}'.")
+            print(
+                f"'{user_answer}' is the wrong answer ;(. "
+                f"The correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {user_name}!")
             break
 
     if counter == 3:
-        print(f'Congratulation, {user_name}!')
+        print(f"Congratulation, {user_name}!")
 
 
 def main():
     main_game_calc()
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     main()
