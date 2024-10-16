c6b1c9c24fbf33ab9b9460c805db5d5ab7f50727
Dana Senko <dana.es@mail.ru>
2024-10-16 18:03:38 +0300

    fix bugs

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -6,7 +6,7 @@ from brain_games.engine import play_game
 def get_question_and_answer_progression():
     start, step, closed_number_index = get_random_for_progression()
     progression = [str(start + step * i) for i in range(10)]
-    correct_answer = progression[closed_number_index]
+    correct_answer = int(progression[closed_number_index])
     progression[closed_number_index] = ".."
     question = " ".join(progression)
 
