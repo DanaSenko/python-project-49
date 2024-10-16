c7ddeb0b751555003d1996c35157020ce4cb1f61
Dana Senko <dana.es@mail.ru>
2024-10-16 17:50:10 +0300

    formatted code

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,13 +1,19 @@
-from brain_games.utils import get_random_for_calc
-from brain_games.consts import RULES_CALC, MATH_SIGNES
 from random import choice
+
 from brain_games.engine import play_game
+from brain_games.consts import RULES_CALC, MATH_SIGNES
+from brain_games.utils import get_random_for_calc
 
 
-def get_math_expression_and_answer():
+def generate_math_expression():
     number1, number2 = get_random_for_calc()
     operation = choice(MATH_SIGNES)
     math_expression = f"{number1} {operation} {number2}"
+    return math_expression
+
+
+def get_math_expression_and_answer():
+    math_expression = generate_math_expression()
     answer = eval(math_expression)
 
     return math_expression, answer
--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,7 +1,8 @@
+from math import gcd
+
 from brain_games.utils import get_random_for_gcd
 from brain_games.consts import RULES_GCD
 from brain_games.engine import play_game
-from math import gcd
 
 
 def get_question_and_answer_gcd():
