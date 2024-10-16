7392918f13e2617f291051046621811f81bb5f4c
Dana Senko <dana.es@mail.ru>
2024-10-16 17:36:32 +0300

    fix file

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,12 +7,11 @@ from brain_games.engine import play_game
 def get_math_expression_and_answer():
     number1, number2 = get_random_for_calc()
     operation = choice(MATH_SIGNES)
-    math_expression =  f"{number1} {operation} {number2}"
+    math_expression = f"{number1} {operation} {number2}"
     answer = eval(math_expression)
 
     return math_expression, answer
-       
+
 
 def play_game_calc():
     play_game(get_math_expression_and_answer, RULES_CALC)
-    
